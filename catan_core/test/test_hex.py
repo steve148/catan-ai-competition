5a332ef309d56e7c6827cf897e671095eaf99dd4
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-06-25 16:37:02 -0400

    chore: update test to match expected usage

--- catan_core/test/test_hex.py
+++ catan_core/test/test_hex.py
@@ -1,8 +1,9 @@
+from catan_core.resource_type.rock import Rock
 from catan_core.hex import Hex
 
 
 class TestHex:
     def test_init(self):
-        hex = Hex("rock", 5)
-        assert hex.resource_type == "rock"
+        hex = Hex(Rock, 5)
+        assert hex.resource_type == Rock
         assert hex.number == 5
