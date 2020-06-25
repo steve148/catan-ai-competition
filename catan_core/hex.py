1abf2dfd840e3fc95bf49ae34782fa49b3a1675a
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-06-25 16:42:35 -0400

    chore: add type hints to hex

--- catan_core/hex.py
+++ catan_core/hex.py
@@ -1,4 +1,7 @@
+from catan_core.resource_type.resource_type import ResourceType
+
+
 class Hex:
-    def __init__(self, resource_type, number):
+    def __init__(self, resource_type: ResourceType, number: int):
         self.resource_type = resource_type
         self.number = number
