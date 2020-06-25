7b6abc1ea0aaaa6f4e90bdf8630a953d02bc2a0f
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-06-24 23:46:11 -0400

    chore: add a few missing tests

--- catan_core/test/test_board.py
+++ catan_core/test/test_board.py
@@ -6,3 +6,9 @@ class TestBoard:
 
     def test_nineteen_hexes(self):
         assert len(Board().hexes) == 19
+
+    def test_fifty_four_vertices(self):
+        assert len(Board().vertices) == 54
+
+    def test_seventy_two_edges(self):
+        assert len(Board().edges) == 72
