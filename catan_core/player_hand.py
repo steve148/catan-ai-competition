551296790be220787d4d380019d4b20e83cd68f8
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-08-08 18:39:34 -0400

    feat: add class representing player hand of resources

---
+++ catan_core/player_hand.py
@@ -0,0 +1,36 @@
+from catan_core.player.player import Player
+from catan_core.resource_type.clay import Clay
+from catan_core.resource_type.resource_type import ResourceType
+from catan_core.resource_type.rock import Rock
+from catan_core.resource_type.sheep import Sheep
+from catan_core.resource_type.wheat import Wheat
+from catan_core.resource_type.wood import Wood
+
+
+class PlayerHand:
+    def __init__(self, player: Player):
+        self.player = player
+        self.hand = {}
+        for resource_type in [Clay, Rock, Sheep, Wheat, Wood]:
+            self.hand[resource_type] = 0
+
+    def has(self, resource_type: ResourceType, count: int) -> bool:
+        return self.hand[resource_type] >= count
+
+    def add(self, resource_type: ResourceType, count: int) -> None:
+        if not count > 0:
+            raise RuntimeError("Cannot add 0 or less cards to the hand.")
+
+        self.hand[resource_type] += count
+
+    def remove(self, resource_type: ResourceType, count: int) -> int:
+        if not count > 0:
+            raise RuntimeError("Cannot remove 0 or less cards from the hand.")
+
+        if self.hand[resource_type] < count:
+            count_removed = self.hand[resource_type]
+            self.hand[resource_type] = 0
+            return count_removed
+        else:
+            self.hand[resource_type] -= count
+            return count
---
+++ catan_core/test/test_player_hand.py
@@ -0,0 +1,75 @@
+import pytest
+
+from catan_core.player.player import Player
+from catan_core.player_hand import PlayerHand
+from catan_core.resource_type.clay import Clay
+from catan_core.resource_type.rock import Rock
+from catan_core.resource_type.sheep import Sheep
+from catan_core.resource_type.wheat import Wheat
+from catan_core.resource_type.wood import Wood
+
+
+class TestPlayerHand:
+    def test_init_player(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        assert player_hand.player == player
+
+    def test_init_hand(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        assert player_hand.hand[Clay] == 0
+        assert player_hand.hand[Rock] == 0
+        assert player_hand.hand[Sheep] == 0
+        assert player_hand.hand[Wheat] == 0
+        assert player_hand.hand[Wood] == 0
+
+    def test_has_at_least_x_of_resource_type(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        player_hand.hand[Clay] = 2
+        assert player_hand.has(resource_type=Clay, count=2)
+
+    def test_has_less_than_x_of_resource_type(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        player_hand.hand[Clay] = 2
+        assert not player_hand.has(resource_type=Clay, count=3)
+
+    def test_add_raises_if_count_less_than_1(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        with pytest.raises(
+            RuntimeError, match="Cannot add 0 or less cards to the hand."
+        ):
+            player_hand.add(resource_type=Clay, count=0)
+
+    def test_add_resources_to_hand(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        player_hand.add(resource_type=Clay, count=10)
+        assert player_hand.hand[Clay] == 10
+
+    def test_remove_raises_if_count_less_than_1(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        with pytest.raises(
+            RuntimeError, match="Cannot remove 0 or less cards from the hand."
+        ):
+            player_hand.remove(resource_type=Clay, count=0)
+
+    def test_remove_has_at_least_x_of_resource_type(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        player_hand.hand[Clay] = 2
+        removed_count = player_hand.remove(resource_type=Clay, count=1)
+        assert removed_count == 1
+        assert player_hand.hand[Clay] == 1
+
+    def test_remove_has_less_than_x_of_resource_type(self):
+        player = Player()
+        player_hand = PlayerHand(player=player)
+        player_hand.hand[Clay] = 2
+        removed_count = player_hand.remove(resource_type=Clay, count=3)
+        assert removed_count == 2
+        assert player_hand.hand[Clay] == 0
