2c7201907c80f264fe7b6a2a15e57ce8ac8234fa
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2020-12-31 10:18:21 -0500

    feat: can place building on vertex

--- catan_core/test/test_vertex.py
+++ catan_core/test/test_vertex.py
@@ -1,7 +1,11 @@
 import pytest
 
 from catan_core.building.building import Building
+from catan_core.building.settlement import Settlement
+from catan_core.edge import Edge
+from catan_core.player.player import Player
 from catan_core.port.rock_port import RockPort
+from catan_core.road import Road
 from catan_core.vertex import Vertex
 
 
@@ -30,3 +34,52 @@ class TestVertex:
         building = Building()
         vertex.assign_building(building=building)
         assert vertex.building == building
+
+    def test_can_place_building_returns_false_if_buidling_exists(self):
+        player = Player()
+        vertex = Vertex()
+
+        vertex.building = Settlement(player=player)
+
+        assert not vertex.can_place_building(player=player)
+
+    def test_can_place_building_returns_false_if_no_roads_exist(self):
+        player = Player()
+        different_player = Player()
+        vertex = Vertex()
+        edge_no_road = Edge()
+        edge_road_different_player = Edge()
+
+        vertex.edges = [edge_no_road, edge_road_different_player]
+        edge_no_road.vertices = [vertex]
+        edge_road_different_player.vertices = [vertex]
+        edge_road_different_player.road = Road(player=different_player)
+
+        assert not vertex.can_place_building(player=player)
+
+    def test_can_place_building_Returns_false_if_building_is_nearby(self):
+        player = Player()
+        vertex = Vertex()
+        neighbour_vertex = Vertex()
+        edge = Edge()
+
+        vertex.edges = [edge]
+        neighbour_vertex.edges = [edge]
+        neighbour_vertex.building = Settlement(player=player)
+        edge.vertices = [vertex, neighbour_vertex]
+        edge.road = Road(player=player)
+
+        assert not vertex.can_place_building(player=player)
+
+    def test_can_place_building_returns_true(self):
+        player = Player()
+        vertex = Vertex()
+        edge = Edge()
+        neighbour_vertex = Vertex()
+
+        edge.road = Road(player)
+        vertex.edges = [edge]
+        neighbour_vertex.edges = [edge]
+        edge.vertices = [vertex, neighbour_vertex]
+
+        assert vertex.can_place_building(player=player)
--- catan_core/vertex.py
+++ catan_core/vertex.py
@@ -1,7 +1,8 @@
-from typing import List
+from typing import List, Optional
 
 from catan_core.building.building import Building
 from catan_core.edge import Edge
+from catan_core.player.player import Player
 from catan_core.port.port import Port
 
 
@@ -9,10 +10,25 @@ class Vertex:
     def __init__(self, port: Port = None):
         self.edges: List[Edge] = []
         self.port = port
-        self.building = None
+        self.building: Optional[Building] = None
 
     def assign_building(self, building: Building):
         if self.building:
             raise RuntimeError("A building already exists at this vertex")
 
         self.building = building
+
+    def can_place_building(self, player: Player) -> bool:
+        if self.building:
+            return False
+
+        if all(not edge.road or edge.road.player != player for edge in self.edges):
+            return False
+
+        # Buildings must be at least 2 edges apart.
+        for edge in self.edges:
+            for vertex in edge.vertices:
+                if vertex.building:
+                    return False
+
+        return True
