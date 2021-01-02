18fe1dcf97ba0b36f90b237ffcfbd73747580ed2
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2021-01-02 09:22:24 -0500

    feat: action kwargs can be empty

--- catan_core/action.py
+++ catan_core/action.py
@@ -1,4 +1,4 @@
 class Action:
-    def __init__(self, name: str, kwargs: dict) -> None:
+    def __init__(self, name: str, kwargs: dict = None) -> None:
         self.name = name
         self.kwargs = kwargs
--- catan_core/test/test_action.py
+++ catan_core/test/test_action.py
@@ -3,6 +3,11 @@ from catan_core.action import Action
 
 class TestAction:
     def test_init(self):
+        action = Action(name="build_something")
+        assert action.name == "build_something"
+        assert not action.kwargs
+
+    def test_init_with_kwargs(self):
         action = Action(name="build_something", kwargs={"key": "value"})
         assert action.name == "build_something"
         assert action.kwargs == {"key": "value"}
