1b8247ab858273db18e818e227999f9f614cc8c7
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2021-01-02 09:36:30 -0500

    feat: add equality function to action

--- catan_core/action.py
+++ catan_core/action.py
@@ -2,3 +2,6 @@ class Action:
     def __init__(self, name: str, kwargs: dict = None) -> None:
         self.name = name
         self.kwargs = kwargs
+
+    def __eq__(self, o: object) -> bool:
+        return self.name == o.name and self.kwargs == o.kwargs
--- catan_core/test/test_action.py
+++ catan_core/test/test_action.py
@@ -11,3 +11,15 @@ class TestAction:
         action = Action(name="build_something", kwargs={"key": "value"})
         assert action.name == "build_something"
         assert action.kwargs == {"key": "value"}
+
+    def test_eq_returns_false_if_name_different(self):
+        assert Action(name="a") != Action(name="b")
+
+    def test_eq_returns_false_if_kwargs_different(self):
+        assert Action(name="a", kwargs={"a": 1}) != Action(name="a", kwargs={"a": 2})
+
+    def test_eq_returns_true_if_name_is_equal(self):
+        assert Action(name="x") == Action(name="x")
+
+    def test_eq_returns_true_if_name_and_kwargs_are_equal(self):
+        assert Action(name="x", kwargs={"a": 1}) == Action(name="x", kwargs={"a": 1})
