ffbc6d66621aada512afabd12051f1932cfdb0a2
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2021-01-10 13:56:12 -0500

    feat: road equality

--- catan_core/road.py
+++ catan_core/road.py
@@ -4,3 +4,6 @@ from catan_core.player.player import Player
 class Road:
     def __init__(self, player: Player):
         self.player = player
+
+    def __eq__(self, o: object) -> bool:
+        return self.player == o.player
--- catan_core/test/test_road.py
+++ catan_core/test/test_road.py
@@ -7,3 +7,12 @@ class TestRoad:
         player = Player()
         road = Road(player=player)
         assert road.player == player
+
+    def test_eq_returns_false(self):
+        player = Player()
+        other_player = Player()
+        assert Road(player=player) != Road(player=other_player)
+
+    def test_eq_returns_true(self):
+        player = Player()
+        assert Road(player=player) == Road(player=player)
